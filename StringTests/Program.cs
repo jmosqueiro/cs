fbfd7003d9814cc9ed34037bdddca9e54b74b767
jmosqueiro <furosktim@icloud.com>
2021-02-15 20:55:47 +0000

    Hello world folder

---
+++ StringTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "test";
+            string msg = name + " is done";
+
+            System.Console.WriteLine(msg);
+            
+            string toUpperMsg = msg.ToUpper();
+            System.Console.WriteLine(toUpperMsg);
+            Console.ReadLine();
+            
+        }
+    }
+}
